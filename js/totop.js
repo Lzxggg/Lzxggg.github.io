53be2b3b4c67ca4877acb46ca04a9ba9fcf373f7
your name <your email>
2019-06-05 09:47:52 +0800

    Site updated: 2019-06-05 09:47:51

--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,13 @@
 let totopblock = document.querySelector('.totop');
 
 
-totopblock.onclick = toTop;
+
 function toTop() {
     window.scrollTo({ top: 0, behavior: 'auto' });    // instant, smooth, auto
     totopblock.style.visibility = 'hidden';
 }
 
+totopblock.onclick = toTop;
 
 window.addEventListener('scroll', () => {
 
